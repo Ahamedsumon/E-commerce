dd268a21cbad1500b6609d5b49e7a7e687cd7136
Ahamed Sumon <ahamedsumon13579@gmail.com>
2025-06-27 15:39:28 +0300

    added dynamically phone items in display

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('connected')
+const loadPhone = async() => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones)
+    displayPhone(phones)
+
+}
+
+const displayPhone = phones => {
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone)
+
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 w-96 shadow-sm`;
+        phoneCard.innerHTML = `
+        <figure>
+    <img
+      src="${phone.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${phone.phone_name
+}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `
+        phoneContainer.appendChild(phoneCard)
+
+    })
+}
+
+loadPhone()
